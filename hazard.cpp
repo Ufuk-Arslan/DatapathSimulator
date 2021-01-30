ec58062ea4b604373552f9fce8d3de615dd5772e
Ufuk-Arslan <ufukarslan99@gmail.com>
2021-01-30 20:10:53 +0300

    Change for forwarding

--- hazard.cpp
+++ hazard.cpp
@@ -2,6 +2,14 @@
 
 void Datapath::hazard(void){
 
+    if(MEMWB.destReg_o != 0 
+    && !(EXMEM.destReg_o != 0
+        && (EXMEM.destReg_o = IDEX.src1Reg_o))
+    && (MEMWB.destReg_o = IDEX.src1Reg_o) IDEX.src1Reg_o = EXMEM.result_o; //Enabling mux to get register value directly form WB to EX for the first register
 
+    if(MEMWB.destReg_o != 0 
+    && !(EXMEM.destReg_o != 0
+        && (EXMEM.destReg_o = IDEX.src2Reg_o))
+    && (MEMWB.destReg_o = IDEX.src2Reg_o) IDEX.src2Reg_o = EXMEM.result_o; //Enabling mux to get register value directly form WB to EX for the second register
 
 }
