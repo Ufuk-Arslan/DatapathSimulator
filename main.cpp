03bb698860fc3965001dc1253b1135851c8189fd
Selman Berk Özkurt <44111838+SelmanB@users.noreply.github.com>
2021-01-30 12:24:21 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "datapath.h"
+#include "fetch.cpp"
+#include "decode.cpp"
+#include "execute.cpp"//should include forwarding
+#include "mem.cpp"
+#include "wb.cpp"
+
+#include "control.cpp"
+#include "hazard.cpp"
+
+
+
+int main(int argc, char** argv){
+  //Assemble asm input
+  
+
+
+
+
+
+}
